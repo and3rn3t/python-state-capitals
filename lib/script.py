656b7cbddaf949350237fd6e719678d99439006b
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-09-14 17:28:11 -0500

    have working game, will work on randomizer

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,23 @@
 from capitals import states
+import random
 
-print(states)
+score = 0
+print("Thanks for playing State Capitals!")
+print("Guess the correct state capital, or 'quit' to end the game.")
+
+for i in range(50):
+    rand_state = (random.randint(0,49))
+    # print(states[rand_state]['name'])
+    # print(states[rand_state]['capital'])
+    
+    answer = input(f"What is the capital of {states[rand_state]['name']}? ('quit' to end game): ")
+
+    if answer == states[rand_state]['capital']:
+        print("Correct!")
+        score = score + 1
+        print(f"You got one! You now have {score} points.")
+    elif answer == 'quit':
+        print(f"Thanks for playing! You finished with {score} points, great job.")
+        break
+    else:
+        print(f"Incorrect! The capital of {states[rand_state]['name']} is {states[rand_state]['capital']}")
