fe931ffcd94fe6c6a3803020faee86b72186c7fa
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-09-15 09:21:59 -0500

    made a few format changes

--- lib/script.py
+++ lib/script.py
@@ -2,6 +2,7 @@ from capitals import states
 import random
 
 score = 0
+wrong = 0
 print("Thanks for playing State Capitals!")
 print("Guess the correct state capital, or 'quit' to end the game.")
 
@@ -15,9 +16,11 @@ for i in range(50):
     if answer == states[rand_state]['capital']:
         print("Correct!")
         score = score + 1
-        print(f"You got one! You now have {score} points.")
+        print(f"You got one! Score: {score} correct and {wrong} incorrect.")
     elif answer == 'quit':
-        print(f"Thanks for playing! You finished with {score} points, great job.")
+        print(f"Thanks for playing! You finished with {score} correct answers, and {wrong} incorrect. Great job!")
         break
     else:
+        wrong = wrong + 1
         print(f"Incorrect! The capital of {states[rand_state]['name']} is {states[rand_state]['capital']}")
+        print(f"You currently have {score} correct answers and {wrong} incorrect answers.")
